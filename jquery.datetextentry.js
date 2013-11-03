4f339950dd6cb4157c855b4e720351b302d60ccd
Grant McLean <grant@mclean.net.nz>
2013-11-03 18:22:47 +1300

    bump version for release

--- jquery.datetextentry.js
+++ jquery.datetextentry.js
@@ -1,5 +1,5 @@
 /*
- * jQuery datetextentry v2.0.2
+ * jQuery datetextentry v2.0.3
  * Copyright (c) 2010-2013 Grant McLean (grant@mclean.net.nz)
  *
  * Source repo: https://github.com/grantm/jquery-datetextentry
